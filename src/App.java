09518a837b70d6ca2bd5de0138b7d977ed4ac07a
MillieLerner <28mlerner@gannacademy.org>
2026-02-11 13:35:37 -0500

    Done

--- src/App.java
+++ src/App.java
@@ -10,5 +10,32 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-    }
-}
+        int largest=0;
+        int smallest=0;
+        while(true){
+            System.out.println("What is the test score?");
+            int number=Integer.valueOf(scanner.nextLine());
+            if(number==-1){
+                break;
+            } else if (number>=largest){
+                    largest=number;
+                } else{
+                    smallest=number;
+                }
+                }
+            System.out.println("The high score was "+largest+". The lowest score was "+smallest);
+                }
+            }
+        
+
+
+   
+
+        
+// public static int smallest(int number1, int number2) {
+//         if (number1 > number2) {
+//             return number2;
+//         } else {
+//             return number1;
+
+//         }
